b7265a4a568b94900d7691f200eb2dde67e91751
noushad970 <ug2102038@cse.pstu.ac.bd>
2024-04-11 00:42:21 +0600

    adding waypoint for AI characters

---
+++ TrafficAI/WayPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    [Header("Waypoint Status")]
+    public WayPoint previousWayPoint;
+    public WayPoint nextWayPoint;
+
+    [Range(0f, 5f)]
+    public float waypointWidth;
+
+    public Vector3 GetPosition()
+    {
+        Vector3 minBound = transform.position + transform.right * waypointWidth / 2f;
+        Vector3 maxBound = transform.position - transform.right * waypointWidth / 2f;
+        return Vector3.Lerp(minBound, maxBound, Random.Range(0f, 1f));
+    }
+}
